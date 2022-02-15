8f28536b0f38cfa91f44e381d5d5de8dc550fd43
Fahad <58299077+csMrFahad@users.noreply.github.com>
2022-02-16 01:14:17 +0600

    base logic

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext('2d');
 // ctx holds reference to the canvas renderer context; we will use it to draw
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
+const particlesArray = [];
 
 window.addEventListener('resize', ()=>{
     canvas.height = window.innerHeight;
@@ -53,11 +54,47 @@ canvas.addEventListener('mousemove', (event)=> {
     drawCircle();
 })
 
+class Particle {
+    constructor(){
+        // this.x = mouse.x;
+        // this.y = mouse.y;
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.size = Math.random() * 5 + 1;
+        this.speedX = Math.random()*3- 1.5;
+        this.speedY = Math.random()*3- 1.5;
+    }
+    update(){
+        this.x +=this.speedX;
+        this.y +=this.speedY;
+
+    }
+    draw(){
+        ctx.fillStyle = 'white';
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, 10, 0, Math.PI*2)
+        ctx.fill();
+    }
+}
+
+function init(){
+    for(let i =0; i<100; i++){
+        particlesArray.push(new Particle());
+    }
+}
+init();
+
+function handleParticles(){
+    for(let i=0; i<particlesArray.length; i++){
+        particlesArray[i].update();
+        particlesArray[i].draw();
+    }
+}
 
 function animate(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
+    handleParticles();
     // loop animate
-    drawCircle();
     requestAnimationFrame(animate);
 }
 animate();
