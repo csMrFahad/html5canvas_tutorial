4a6b45a9ae8365d789755a96c206328dc7055af1
Fahad <58299077+csMrFahad@users.noreply.github.com>
2022-02-16 01:39:49 +0600

    last

--- script.js
+++ script.js
@@ -28,14 +28,16 @@ window.addEventListener('resize', ()=>{
 
 
 const mouse = {
-    x: null,
-    y: null,
+    x: undefined,
+    y: undefined,
 }
 
 canvas.addEventListener('click', (event)=> {
     mouse.x = event.x;
     mouse.y = event.y;
-    drawCircle();
+    for(let i=0; i<10;i++){
+        particlesArray.push(new Particle());
+    }
 })
 
 function drawCircle() {
@@ -51,43 +53,44 @@ function drawCircle() {
 canvas.addEventListener('mousemove', (event)=> {
     mouse.x = event.x;
     mouse.y = event.y;
-    drawCircle();
+    for(let i=0; i<1;i++){
+        particlesArray.push(new Particle());
+    }
 })
 
 class Particle {
     constructor(){
-        // this.x = mouse.x;
-        // this.y = mouse.y;
-        this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height;
-        this.size = Math.random() * 5 + 1;
+        this.x = mouse.x;
+        this.y = mouse.y;
+        // this.x = Math.random() * canvas.width;
+        // this.y = Math.random() * canvas.height;
+        this.size = Math.random() * 10 + 1;
         this.speedX = Math.random()*3- 1.5;
         this.speedY = Math.random()*3- 1.5;
     }
     update(){
         this.x +=this.speedX;
         this.y +=this.speedY;
-
+        if (this.size>0.2) this.size -= 0.1;
     }
     draw(){
         ctx.fillStyle = 'white';
         ctx.beginPath();
-        ctx.arc(this.x, this.y, 10, 0, Math.PI*2)
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI*2)
         ctx.fill();
     }
 }
 
-function init(){
-    for(let i =0; i<100; i++){
-        particlesArray.push(new Particle());
-    }
-}
-init();
+
 
 function handleParticles(){
     for(let i=0; i<particlesArray.length; i++){
         particlesArray[i].update();
         particlesArray[i].draw();
+        if(particlesArray[i].size <= 0.3){
+            particlesArray.splice(i,1);
+            i--;
+        }
     }
 }
 
